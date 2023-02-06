fb42cbcf441a2ac1bb7d15f23f46c277adca5e8d
Alon Lavie <alonlavie@am.local>
2023-02-06 12:59:19 -0500

    finished project

--- hrd.py
+++ hrd.py
@@ -1,3 +1,6 @@
+import argparse
+from queue import PriorityQueue
+
 WIDTH, HEIGHT = 4, 5
 
 def create_grid_from_file(filename):
@@ -79,10 +82,154 @@ def get_point_successors_double(grid):
                     successors.append(curr)
     return successors
 
-def get_point_succesors(grid):
+def get_point_successors(grid):
     return get_point_successors_single(grid) + get_point_successors_double(grid)
 
-if __name__ == '__main__':
-    grid = create_grid_from_file('tests/test-input-file-3')
-    for grid in get_point_succesors(grid):
-        print(display_grid(grid))
+def get_vertical_successors(grid):
+    successors = []
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            if i != HEIGHT - 1 and grid[i][j] == '.' and grid[i+1][j] == '.':
+                if j != 0 and grid[i][j - 1] == '^':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i][j - 1], curr[i + 1][j - 1], curr[i][j], curr[i + 1][j] = '.', '.', '^', 'v'
+                    successors.append(curr)
+                if j != WIDTH - 1 and grid[i][j+1] == '^':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i][j + 1], curr[i + 1][j + 1], curr[i][j], curr[i + 1][j] = '.', '.', '^', 'v'
+                    successors.append(curr)
+                if j != 0 and grid[i][j-1] == '1' and grid[i+1][j-1] == '1':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i][j - 2], curr[i + 1][j - 2] = '.', '.'
+                    curr[i][j - 1], curr[i + 1][j - 1] = '1', '1'
+                    curr[i][j], curr[i + 1][j] = '1', '1'
+                    successors.append(curr)
+                if j != WIDTH - 1 and grid[i][j+1] == '1' and grid[i+1][j+1] == '1':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i][j + 2], curr[i + 1][j + 2] = '.', '.'
+                    curr[i][j + 1], curr[i + 1][j + 1] = '1', '1'
+                    curr[i][j], curr[i + 1][j] = '1', '1'
+                    successors.append(curr)
+    return successors
+
+def get_horizontal_successors(grid):
+    successors = []
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            if j != WIDTH - 1 and grid[i][j] == '.' and grid[i][j+1] == '.':
+                if i != 0 and grid[i-1][j] == '<':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i - 1][j], curr[i - 1][j + 1], curr[i][j], curr[i][j + 1] = '.', '.', '<', '>'
+                    successors.append(curr)
+                if i != HEIGHT - 1 and grid[i+1][j] == '<':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i + 1][j], curr[i + 1][j + 1], curr[i][j], curr[i][j + 1] = '.', '.', '<', '>'
+                    successors.append(curr)
+                if i != 0 and grid[i-1][j] == '1' and grid[i-1][j+1] == '1':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i - 2][j], curr[i - 2][j + 1] = '.', '.'
+                    curr[i - 1][j], curr[i - 1][j + 1] = '1', '1'
+                    curr[i][j], curr[i][j + 1] = '1', '1'
+                    successors.append(curr)
+                if i != HEIGHT - 1 and grid[i+1][j] == '1' and grid[i+1][j+1] == '1':
+                    curr = []
+                    for k in range(HEIGHT):
+                        curr.append(grid[k].copy())
+                    curr[i + 2][j], curr[i + 2][j + 1] = '.', '.'
+                    curr[i + 1][j], curr[i + 1][j + 1] = '1', '1'
+                    curr[i][j], curr[i][j + 1] = '1', '1'
+                    successors.append(curr)
+    return successors
+
+def get_successors(grid):
+    return get_vertical_successors(grid) + get_horizontal_successors(grid) + get_point_successors(grid)
+
+def is_goal(grid):
+    return grid[3][1] == '1' and grid[3][2] == '1' and grid[4][1] == '1' and grid[4][2] == '1'
+
+def get_manhattan_distance(grid):
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            if grid[i][j] == '1':
+                return abs(i - 3) + abs(j - 1)
+
+def run_dfs(grid):
+    stack = [grid]
+    while len(stack) > 0:
+        curr = stack.pop()
+        if is_goal(curr):
+            return curr
+        for successor in get_successors(curr):
+            stack.append(successor)
+    return None
+
+def run_astar(grid, debug=False):
+    queue = PriorityQueue()
+    moves = 0
+    queue.put((get_manhattan_distance(grid), (grid, moves)))
+    seen = set()
+    while not queue.empty():
+        curr, moves = queue.get()[1]
+        if display_grid(curr) in seen:
+            continue
+        seen.add(display_grid(curr))
+        if is_goal(curr):
+            return curr
+        if debug:
+            print(display_grid(curr), moves)
+        for successor in get_successors(curr):
+            queue.put((get_manhattan_distance(successor) + moves + 1, (successor, moves + 1)))
+    return None
+
+def run_search(algo, grid):
+    if algo == 'dfs':
+        return run_dfs(grid)
+    elif algo == 'astar':
+        return run_astar(grid)
+
+def output_file(filename, input_grid, output_grid):
+    with open(f"{filename}.txt", "w") as file:
+        file.write(f"{display_grid(input_grid)}\n{display_grid(output_grid)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inputfile",
+        type=str,
+        required=True,
+        help="The input file that contains the puzzle."
+    )
+    parser.add_argument(
+        "--outputfile",
+        type=str,
+        required=True,
+        help="The output file that contains the solution."
+    )
+    parser.add_argument(
+        "--algo",
+        type=str,
+        required=True,
+        choices=['astar', 'dfs'],
+        help="The searching algorithm."
+    )
+    args = parser.parse_args()
+    # read the board from the file
+    inp = create_grid_from_file(args.inputfile)
+    # run specified algorithm on board
+    outp = run_search(args.algo, inp)
+    # write results to output file (create it if it is missing)
+    output_file(args.outputfile, inp, outp)
